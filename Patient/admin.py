91268f0090813288acee7ba0c2bdd427e342b4c4
unknown <arnauldkayonga1@gmail.com>
2022-09-04 18:43:21 +0200

    New db with models

--- Patient/admin.py
+++ Patient/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(Users)
+admin.site.register(clinics)
+admin.site.register(Consultations)
--- Patient/models.py
+++ Patient/models.py
@@ -2,17 +2,6 @@ from django.db import models
 from .managers import CustomUserManager
 from django.contrib.auth.models import AbstractUser, User
 
-class consultations(models.Model):
-    consultation_id = models.SmallAutoField(db_column='Consultation_ID', primary_key=True, editable=False)
-
-
-    class Meta:
-        db_table = 'Consultations'
-        verbose_name_plural = "Consultations"
-
-    def __str__(self):
-        return self.consultation_id
-
 
 class Users(AbstractUser):
     user_id = models.SmallAutoField(primary_key=True, unique=True, editable=False)
@@ -33,3 +22,33 @@ class Users(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class clinics(models.Model):
+    clinic_id = models.SmallAutoField(db_column='clinic_id', primary_key=True, editable=False)
+    name = models.CharField(db_column='Name', max_length=50)
+
+
+
+    class Meta:
+        db_table = 'Clinic'
+        verbose_name_plural = "Clinics"
+
+    def __str__(self):
+        return self.clinic_id
+
+
+
+class Consultations(models.Model):
+    consultation_id = models.SmallAutoField(db_column='Consultation_ID', primary_key=True, editable=False)
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE, blank=True, null=True)
+    date =  models.DateTimeField(db_column='Datetime', blank=True, null=True)
+    clinic_id = None
+
+
+    class Meta:
+        db_table = 'Consultations'
+        verbose_name_plural = "Consultations"
+
+    def __str__(self):
+        return self.consultation_id
